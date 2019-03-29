2239be4a79ca3ead0c55e80689780468d7433758
czarjulius <juliusczar.jc@gmail.com>
2019-03-29 16:27:39 +0100

    feat(login): a registered  user can login
    ensured that all inputs are validated
    
    [Delivered #164844682]

--- server/middlewares/validateUser.js
+++ server/middlewares/validateUser.js
@@ -4,6 +4,8 @@ import db from '../models/user';
 
 const validateSignup = [
   check('firstName')
+    .not().isEmpty()
+    .withMessage('Firstname is required')
     .matches(/^[a-zA-Z]+$/i)
     .withMessage('firstname must contain only alphabets')
     .isLength({ min: 3 })
@@ -15,6 +17,8 @@ const validateSignup = [
     .trim(),
 
   check('lastName')
+    .not().isEmpty()
+    .withMessage('lastName is required')
     .matches(/^[a-zA-Z]+$/i)
     .withMessage('lastname must contain only alphabets')
     .isLength({ min: 3 })
@@ -25,7 +29,10 @@ const validateSignup = [
     .withMessage('lastname cannot contain whitespaces')
     .trim(),
 
-  check('password').isLength({ min: 6 })
+  check('password')
+    .not().isEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 6 })
     .withMessage('password must have atleast 6 characters')
     .isLength({ max: 50 })
     .withMessage('password cannot contain more than 15 characters')
@@ -34,6 +41,8 @@ const validateSignup = [
     .trim(),
 
   check('phoneNumber')
+    .not().isEmpty()
+    .withMessage('Phone number is required')
     .isLength({ min: 10 })
     .withMessage('please input a valid phone number')
     .isLength({ max: 15 })
@@ -43,16 +52,22 @@ const validateSignup = [
     .trim(),
 
   check('type')
+    .not().isEmpty()
+    .withMessage('Type is required')
     .isIn(['staff', 'user', 'STAFF', 'USER'])
     .withMessage('only staff or user types are allowed')
     .trim(),
 
   check('isAdmin')
+    .not().isEmpty()
+    .withMessage('isAdmin is required')
     .isIn(['true', 'false', 'TRUE', 'FALSE'])
     .withMessage('only true or false are allowed')
     .trim(),
 
   check('email')
+    .not().isEmpty()
+    .withMessage('Email is required')
     .isEmail()
     .withMessage('Please input a valid email address')
     .custom(value => Promise.resolve(db.filter(user => user.email === value)).then((user) => {
@@ -70,4 +85,31 @@ const validateSignup = [
   },
 ];
 
-export default validateSignup;
+const validateLogin = [
+  check('email')
+    .not().isEmpty()
+    .withMessage('email is required')
+    .isEmail()
+    .withMessage('please input a valid email address')
+    .trim(),
+
+  check('password')
+    .not().isEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 3 })
+    .withMessage('please input a valid password')
+    .trim(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        status: 400,
+        error: errors.array().map(i => i.msg),
+      });
+    }
+    next();
+  },
+];
+
+export { validateSignup, validateLogin };
